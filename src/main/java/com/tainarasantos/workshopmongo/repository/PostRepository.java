145ba58ef933f9909cbde11c6cbb4ca61d295466
Tainara Santos <tainara.silva.santos@hotmail.com>
2024-01-10 19:48:04 -0300

    Criando entity Post com user aninhado.

---
+++ src/main/java/com/tainarasantos/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.tainarasantos.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tainarasantos.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
