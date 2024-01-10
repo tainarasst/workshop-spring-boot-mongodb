ce8d43b9b86e1030b7f2748affffd86e07642948
Tainara Santos <tainara.silva.santos@hotmail.com>
2024-01-10 15:33:09 -0300

    Obtendo um usuario por id.

---
+++ src/main/java/com/tainarasantos/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.tainarasantos.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+}
